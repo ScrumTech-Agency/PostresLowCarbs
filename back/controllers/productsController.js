ee5c93879dfe6d55641e2f2c1e84dbb66ca7809d
Erik Moreno <erikmoreno790@gmail.com>
2022-10-24 16:12:50 -0500

    Adicion de controlador para modificar un producto

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -4,3 +4,26 @@ exports.getProducts=(req,res,next) =>{
     message: "Aqui podra ver todos los productos"
   })
 }
+
+//Actualizar un producto
+exports.updateProduct= async(req,res,next) =>{
+  let product = await producto.findById(req.params.id);
+  if (!product){
+      return res.status(404).json({
+          success:false,
+          message: "No se encontró el producto"
+      })
+  } 
+  // Si existe el objeto, entonces se ejecuta la actualizacion
+  product = await producto.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, //Valido solo los atributos nuevos o actualizacion
+      runValidators: true
+  });
+
+  //Respondo OK si el producto se actualizó
+  res.status(200).json({
+      success: true,
+      message: "Producto actualizado exitosamente",
+      product
+})
+}
