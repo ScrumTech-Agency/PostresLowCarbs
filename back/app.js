6e70eec2d49720cd9b9fb4062cb88c854e30ec53
Adriana <tintodsgn@gmail.com>
2022-10-24 15:06:49 -0700

    Conexion BD
    Mongo

--- back/app.js
+++ back/app.js
@@ -1,10 +1,10 @@
 const express=require("express")
 const app = express();
 app.use(express.json());
-
+// importar rutas
 const productos=require("./routes/products")
 
-app.use('api',productos)
+app.use('/api',productos) //ruta del navegador
 
 
 module.exports=app
--- back/server.js
+++ back/server.js
@@ -1,7 +1,16 @@
 const app=require("./app")
+app.listen (process.env.PORT)
+
 const dotenv=require("dotenv");
+
 dotenv.config({path: 'back/config/config.env'})
 
-const server=app.listen(process.env.PORT,() => {
+// configurar  base de datos
+
+
+
+
+const server=app.listen(process.env.PORT, () => {
   console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+  
 }) 
