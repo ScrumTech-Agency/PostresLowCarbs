6f4e3ef29e7f233df09882c47499b8d1290b16f8
JanetMU <110951352+JanetMU@users.noreply.github.com>
2022-10-24 19:55:34 -0500

    Update productsController.js
    Creación Productos

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,6 +1,18 @@
+const producto=require("../models/productos")
+
 exports.getProducts=(req,res,next) =>{
   res.status(200).json({
     success:true,
     message: "Aqui podra ver todos los productos"
   })
+}
+
+//Crear nuevo producto /api/productos
+exports.newProduct=async(req,res,next)=>{
+    const product= await producto.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
+    })
 }
