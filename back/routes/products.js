c8494e24e602203d978f370e79eb77fbb7036412
JanetMU <110951352+JanetMU@users.noreply.github.com>
2022-10-25 15:43:40 -0500

    Update products.js
    Se completa ruta crear producto

--- back/routes/products.js
+++ back/routes/products.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const router=express.Router();
 
-const {getProducts, deleteProduct} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
+const {getProducts, newProduct, deleteProduct} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
 router.route('/productos').get(getProducts) //Establecemos desde que ruta queremos ver el getProducts
 router.route('/producto/nuevo').post(newProduct);
 router.route('/producto/delete/:id').delete(deleteProduct);
