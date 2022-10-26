a8866037838c4efbecbc47a7717d9fc207db0655
Wilson Arrubla Hoyos <wahs_30@hotmail.com>
2022-10-25 20:56:59 -0500

    Se soluciona problemas con las rutas

--- back/routes/products.js
+++ back/routes/products.js
@@ -1,17 +1,13 @@
 const express=require("express")
 const router=express.Router();
 
-<<<<<<< Updated upstream
-const {getProducts, newProduct, deleteProduct} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
+
+const {getProducts, newProduct, deleteProduct, getProductById } = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
 router.route('/productos').get(getProducts) //Establecemos desde que ruta queremos ver el getProducts
 router.route('/producto/nuevo').post(newProduct);
 router.route('/producto/delete/:id').delete(deleteProduct);
-
-=======
-const {getProducts, newProduct, getProductById} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
-router.route('/productos').get(getProducts) //Establecemos desde que ruta queremos ver el getProducts
-router.route('/producto/nuevo').post(newProduct);//Ruta Método post Establecemois la ruta
 router.route('/producto/:id').get(getProductById); //Ruta para consultar por id
->>>>>>> Stashed changes
+
+
 
 module.exports=router;
