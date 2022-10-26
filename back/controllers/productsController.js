c89b5028f01f47a69b6cf858e7d21297f31c0dee
Wilson Arrubla Hoyos <wahs_30@hotmail.com>
2022-10-25 20:04:52 -0500

    Se realiza CRUD Get por ID y se ajusta conexión a base de datos

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -7,6 +7,24 @@ exports.getProducts=(req,res,next) =>{
   })
 }
 
+//Ver un producto por ID
+exports.getProductById= async (req, res, next)=>{
+  const product= await producto.findById(req.params.id)
+  
+  if (!product){
+          return res.status(404).json({
+          success:false,
+          message: 'No encontramos ese producto'
+      })
+  }
+  res.status(200).json({
+      success:true,
+      message:"Aqui debajo encuentras información sobre tu producto: ",
+      product
+  })
+}
+
+
 //Crear nuevo producto /api/productos
 exports.newProduct=async(req,res,next)=>{
     const product= await producto.create(req.body);
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,10 +1,17 @@
 const express=require("express")
 const router=express.Router();
 
+<<<<<<< Updated upstream
 const {getProducts, newProduct, deleteProduct} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
 router.route('/productos').get(getProducts) //Establecemos desde que ruta queremos ver el getProducts
 router.route('/producto/nuevo').post(newProduct);
 router.route('/producto/delete/:id').delete(deleteProduct);
 
+=======
+const {getProducts, newProduct, getProductById} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
+router.route('/productos').get(getProducts) //Establecemos desde que ruta queremos ver el getProducts
+router.route('/producto/nuevo').post(newProduct);//Ruta Método post Establecemois la ruta
+router.route('/producto/:id').get(getProductById); //Ruta para consultar por id
+>>>>>>> Stashed changes
 
 module.exports=router;
