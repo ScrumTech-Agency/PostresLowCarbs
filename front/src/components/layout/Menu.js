86dbe88b021127fcec492f39820877306263417a
JanetMU <110951352+JanetMU@users.noreply.github.com>
2022-10-28 14:19:13 -0500

    Update Menu.js
    Se modifica estilo menú

--- front/src/components/layout/Menu.js
+++ front/src/components/layout/Menu.js
@@ -4,19 +4,18 @@ export const Menu = () => {
   return (
     <Fragment>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    
+<nav class="class="contenedor d-flex justify-content-evenly">
+      
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="http://localhost:3000/HomeAdmin">Productos</a>
+          <a class="nav-link" aria-current="page" href="http://localhost:3000/HomeAdmin">Lista Productos</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="http://localhost:3000/ProductDetails">Modificar</a>
+          <a class="nav-link" aria-current="page" href="http://localhost:3000/ProductDetails">Modificar Productos</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="http://localhost:3000/Ventas">Ventas</a>
+          <a class="nav-link" aria-current="page" href="http://localhost:3000/Ventas">Lista Ventas</a>
         </li>
       </ul>
     </div>
