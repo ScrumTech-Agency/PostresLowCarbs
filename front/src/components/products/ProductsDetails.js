01079955a39a65fc46c5e85f54276e30613dc0df
Erik Moreno <erikmoreno790@gmail.com>
2022-10-28 12:11:24 -0500

    Correcion definitiva de comentario

--- front/src/components/products/ProductsDetails.js
+++ front/src/components/products/ProductsDetails.js
@@ -33,7 +33,7 @@ export const ProductDetails = () => {
         setQuantity(qty)
     }
     
-    {/*Esta funcion permite aumentar la cantidad de productos que se piensa agregar al carrito */}
+    {/*Esta funcion permite disminuir la cantidad de productos que se piensa agregar al carrito */}
     const decreaseQty = () => {
         const contador = document.querySelector('.count')
 
