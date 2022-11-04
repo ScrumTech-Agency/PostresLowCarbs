fd31615c9c48b4f27d9a4b54bc45a66c882b21e6
Adriana <tintodsgn@gmail.com>
2022-11-04 14:37:42 -0700

    Update Uso protector Funciones Async
    Back/productsController

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,58 +1,97 @@
-const producto=require("../models/productos")
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const producto = require("../models/productos");
+const ErrorHandler = require("../utils/errorHandler");
 
 //Ver la lista de productos
-exports.getProducts=async (req,res,next) =>{
-    const productos= await producto.find();
-    res.status(200).json({
-        success:true,
-        cantidad: productos.length,
-        productos
-    })
-}
+exports.getProducts =catchAsyncErrors( async (req, res, next) => {
+  const productos = await producto.find();
+  if (!productos) {
+    return next(new ErrorHandler("Producto no encontrado", 404))
+  }
+
+  res.status(200).json({
+    success: true,
+    cantidad: productos.length,
+    productos
+  })
+})
 
 //Ver un producto por ID
-exports.getProductById= async (req, res, next)=>{
-  const product= await producto.findById(req.params.id)
-  
-  if (!product){
-          return res.status(404).json({
-          success:false,
-          message: 'No encontramos ese producto',
-          error:true
-      })
+exports.getProductById = catchAsyncErrors( async (req, res, next) => {
+  const product = await producto.findById(req.params.id)
+
+  if (!product) {
+    return next(new ErrorHandler("Producto no encontrado", 404))
   }
+
+  res.status(200).json({
+    success: true,
+    message: "Aqui debajo encuentras información sobre tu producto: ",
+    product
+  })
+})
+
+//Actualizar un producto
+exports.updateProduct= catchAsyncErrors(async (req,res,next) =>{
+  let product= await producto.findById(req.params.id) 
+  if (!product){
+      return next(new ErrorHandler("Producto no encontrado", 404))
+  }
+  
+  //Si el objeto si existia, entonces si ejecuto la actualización
+  product= await producto.findByIdAndUpdate(req.params.id, req.body, {
+      new:true, //Valido solo los atributos nuevos o actualizados
+      runValidators:true
+  });
+  //Respondo Ok si el producto si se actualizó
   res.status(200).json({
       success:true,
-      message:"Aqui debajo encuentras información sobre tu producto: ",
+      message:"Producto actualizado correctamente",
       product
   })
-}
-
+})
 
 //Crear nuevo producto /api/productos
-exports.newProduct=async(req,res,next)=>{
-    const product= await producto.create(req.body);
+exports.newProduct = catchAsyncErrors( async (req, res, next) => {
+    const product = await producto.create(req.body);
 
     res.status(201).json({
-        success:true,
-        product
+      success: true,
+      product
     })
-}
+  })
 
 //Borrar productos por id
-exports.deleteProduct=async(req, res, next)=>{
-  const deleteProduct = await producto.findById (req.params.id);
-  if(!deleteProduct){
-    return res.status(404).json({
-      success: false,
-      mensaje: "El producto que intenta eliminar no existe."
-    })
-  }
-  await deleteProduct.remove();
+exports.deleteProduct = catchAsyncErrors (async (req, res, next) => {
+    const product= await producto.findById(req.params.id)
+    
+    if (!product){
+      return next(new ErrorHandler("Producto no encontrado", 404))
+    
+    }
+    await product.remove();
     res.status(200).json({
       success: true,
-      mensaje: "Eliminación exitosa. Los datos del producto elimiado son: ",
-      deleteProduct
-})
+      mensaje: "Eliminación exitosa. Los datos del producto elimiado son: "
+    
+    })
+  })
+
+// FETCH
+//Ver todos los productos
+function verProductos(){
+  fetch('http://localhost:4000/api/productos')
+  .then(res=>res.json())
+  .then(res=>console.log(res))
+  .catch(err=>console.error(err))
 }
 
+//verProductos(); LLamamos al metodo creado para probar la consulta
+
+//Ver por id
+function verProductoPorID(id){
+  fetch('http://localhost:4000/api/producto/'+id)
+  .then(res=>res.json())
+  .then(res=>console.log(res))
+  .catch(err=>console.error(err))
+}
