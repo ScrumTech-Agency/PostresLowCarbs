d68b09acd9012a08f495dfa9a2211e4a01503b7f
Adriana <tintodsgn@gmail.com>
2022-11-04 14:31:04 -0700

    update app.js
    Enlace y manejo de errores

--- back/app.js
+++ back/app.js
@@ -1,10 +1,16 @@
-const express=require("express")
+const express=require("express");
 const app = express();
+const errorMiddleware= require("./middleware/errors")
+
 app.use(express.json());
+
 // importar rutas
 const productos=require("./routes/products")
 
 app.use('/api',productos) //ruta del navegador
+
+//MiddleWares para manejar errores
+app.use(errorMiddleware)
 
 
 module.exports=app
