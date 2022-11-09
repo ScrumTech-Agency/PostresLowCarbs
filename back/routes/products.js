d56814de3ab8d2f7b9025071d1d0ebb8318c64d4
Adriana <tintodsgn@gmail.com>
2022-11-08 21:15:04 -0800

    Update Routes: AuthenticatedUser, authorizeRoles

--- back/routes/products.js
+++ back/routes/products.js
@@ -2,15 +2,17 @@ const express=require("express")
 const router=express.Router();
 
 
-const {getProducts, newProduct, deleteProduct, getProductById } = require("../controllers/productsController"); //Traemos la respuesta json desde el controlador
-const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+const {getProducts, newProduct, deleteProduct, getProductById, updateProduct } = require("../controllers/productsController"); //Traemos la respuesta json desde el controlador
+const { isAuthenticatedUser, authorizeRoles} = require("../middleware/auth");
+
 
 
 //Test Autenticaciones
-router.route('/productos').get(isAuthenticatedUser, authorizeRoles("admin"), getProducts) //Establecemos desde que ruta queremos ver el getProducts
-router.route('/producto/nuevo').post(newProduct);
-router.route('/producto/delete/:id').delete(deleteProduct);
+router.route('/productos').get( getProducts) //Establecemos desde que ruta queremos ver el getProducts
+router.route('/producto/nuevo').post(isAuthenticatedUser,  authorizeRoles("admin"), newProduct);
+router.route('/producto/delete/:id').delete(isAuthenticatedUser,  authorizeRoles("admin"),deleteProduct);
 router.route('/producto/:id').get(getProductById); //Ruta para consultar por id
+router.route('/producto/:id').put(isAuthenticatedUser,  authorizeRoles("admin"),updateProduct);//Creacion de la ruta de actualizacion
 
 
 
