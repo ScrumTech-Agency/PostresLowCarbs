2c5276f68df0d5f921034d4768803a9ed9e4ee0a
Adriana <tintodsgn@gmail.com>
2022-11-08 19:32:43 -0800

    Ruta de autenticación Usuario

--- back/routes/products.js
+++ back/routes/products.js
@@ -2,9 +2,12 @@ const express=require("express")
 const router=express.Router();
 
 
-const {getProducts, newProduct, deleteProduct, getProductById } = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
+const {getProducts, newProduct, deleteProduct, getProductById } = require("../controllers/productsController"); //Traemos la respuesta json desde el controlador
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
-router.route('/productos').get(getProducts) //Establecemos desde que ruta queremos ver el getProducts
+
+//Test Autenticaciones
+router.route('/productos').get(isAuthenticatedUser, authorizeRoles("admin"), getProducts) //Establecemos desde que ruta queremos ver el getProducts
 router.route('/producto/nuevo').post(newProduct);
 router.route('/producto/delete/:id').delete(deleteProduct);
 router.route('/producto/:id').get(getProductById); //Ruta para consultar por id
