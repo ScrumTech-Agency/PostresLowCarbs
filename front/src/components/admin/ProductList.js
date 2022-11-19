7c5f6c3676eaab800b8a7331b3f3ebedf8a77632
Erik Moreno <erikmoreno790@gmail.com>
2022-11-18 22:40:06 -0500

    Se incorpora funcion que permite eliminar un producto de la lista

--- front/src/components/admin/ProductList.js
+++ front/src/components/admin/ProductList.js
@@ -7,7 +7,7 @@ import Sidebar from './Sidebar'
 
 import { useAlert } from 'react-alert'
 import { useDispatch, useSelector } from 'react-redux'
-import {  clearErrors, getAdminProducts } from '../../actions/productActions'
+import {  clearErrors, deleteProduct, getAdminProducts } from '../../actions/productActions'
 
 const ProductsList = () => {
 
@@ -16,6 +16,14 @@ const ProductsList = () => {
 
     const { loading, error, products } = useSelector(state => state.products);
 
+    const deleteProductHandler = (id) => {
+        const response = window.confirm("¿Desea eliminar este producto?")
+        if (response){
+            dispatch (deleteProduct(id))
+            alert.success("Producto eliminado de forma exitosa")
+            window.location.reload(false)
+        }
+    }
     useEffect(() => {
         dispatch(getAdminProducts());
 
@@ -65,13 +73,14 @@ const ProductsList = () => {
                 acciones: <Fragment>
                     <Link to={`/producto/${product._id}`} className="btn btn-primary py-1 px-2">
                         <i className="fa fa-eye"></i>
-                    </Link><Link to="/" className="btn btn-warning py-1 px-2">
+                    </Link><Link to={`/updateProduct/${product._id}`} className="btn btn-warning py-1 px-2">
                     <i class="fa fa-pencil"></i>
                     </Link>
 
-                    <Link to="/" className="btn btn-danger py-1 px-2">
+                    <button className="btn btn-danger py-1 px-2 ml-2" onClick={() => deleteProductHandler(product._id)}>
                         <i className="fa fa-trash"></i>
-                    </Link>
+                    </button>
+                    
                 </Fragment>
             })
         })
